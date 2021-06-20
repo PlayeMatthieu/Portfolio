a9d598b7273b9bd404164d03e7aab96986d271f3
Matthieu Playe <matthieu.playe@student.howest.be>
2021-06-20 10:22:29 +0200

    Update default layout & Add navabr, footer base

---
+++ components/main/navbarComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <b-navbar>
+      <template #brand>
+        <b-navbar-item tag="router-link" :to="{ path: '/' }">
+          <h2 class="title">Playe.be</h2>
+        </b-navbar-item>
+      </template>
+      <template #end>
+        <b-navbar-item href="#"> Home </b-navbar-item>
+        <b-navbar-item href="#"> About </b-navbar-item>
+        <b-navbar-item href="#"> Contact </b-navbar-item>
+      </template>
+    </b-navbar>
+  </div>
+</template>
