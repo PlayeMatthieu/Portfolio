66c9f6fc4f07a16ddd2d9b3e0a4d09f6ae57a33b
Matthieu Playe <matthieu.playe@student.howest.be>
2021-06-25 17:33:57 +0200

    Set footer always to bottom

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,10 @@
 <template>
-  <div>
-    <!-- NAVBAR -->
-    <navbarComponent />
-    <!-- PAGE CONTENT -->
-    <div class="container">
-      <nuxt />
+  <div id="page-container">
+    <div id="content-wrap">
+      <navbarComponent />
+      <nuxt class="container" />
     </div>
-    <!-- FOOTER -->
-    <footerComponent />
+    <footerComponent id="footer" />
   </div>
 </template>
 
@@ -18,3 +15,19 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+#page-container {
+  position: relative;
+  min-height: 100vh;
+}
+#content-wrap {
+  padding-bottom: 2.5rem; /* Footer height */
+}
+#footer {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 2.5rem; /* Footer height */
+}
+</style>
