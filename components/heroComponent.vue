9221929a937fd425e13bd1ad07feb09c8a040cf9
Matthieu Playe <matthieu.playe@student.howest.be>
2021-06-26 19:29:30 +0200

    update heroComponent

--- components/heroComponent.vue
+++ components/heroComponent.vue
@@ -6,7 +6,7 @@
         <h1 class="hero-title">Student developer</h1>
         <span class="underline"></span>
         <h3 class="hero-subtitle">
-          I’m a passionate <span>webdeveloper.</span>
+          I’m a passionate <span>VueJs & ASP.NET</span> web developer
         </h3>
       </div>
     </div>
