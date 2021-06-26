a1feb7782dafe693cfda17c761f9c09cc80de0bf
Matthieu Playe <matthieu.playe@student.howest.be>
2021-06-26 19:29:56 +0200

    Add aboutComponent

---
+++ components/aboutComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="about">
+    <a href="#" class="about-content">
+      <h2>
+        Pleasure to <br />
+        meet you
+      </h2>
+      <h3 class="subtitle">About me</h3>
+      <div class="about-image">
+        <img src="~/assets/img/Matthieu_lach.png" alt="Matthieu" />
+      </div>
+    </a>
+  </section>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <heroComponent />
+    <aboutComponent />
   </div>
 </template>
 
