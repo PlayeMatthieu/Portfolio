215a3fada9e0fde6c72e947780049c9f56f36c21
Matthieu Playe <matthieu.playe@student.howest.be>
2021-06-26 14:27:52 +0200

    Add navbar & minor fixes

--- components/main/navbarComponent.vue
+++ components/main/navbarComponent.vue
@@ -1,16 +1,47 @@
 <template>
-  <div class="container">
-    <b-navbar>
-      <template #brand>
-        <b-navbar-item tag="router-link" :to="{ path: '/' }">
-          <h2 class="title">Playe.be</h2>
-        </b-navbar-item>
-      </template>
-      <template #end>
-        <b-navbar-item href="#"> Home </b-navbar-item>
-        <b-navbar-item href="#"> About </b-navbar-item>
-        <b-navbar-item href="#"> Contact </b-navbar-item>
-      </template>
-    </b-navbar>
-  </div>
+  <header class="header">
+    <nav class="navbar">
+      <a href="" class="nav-logo">Playe.be</a>
+      <ul class="nav-menu">
+        <li v-for="link in links" :key="link.title" class="nav-item">
+          <a :href="link.href" class="nav-link" @click="closeNavbar">{{
+            link.title
+          }}</a>
+        </li>
+      </ul>
+      <div class="hamburger" @click="toggleNavbar">
+        <span class="bar"></span>
+        <span class="bar"></span>
+        <span class="bar"></span>
+      </div>
+    </nav>
+  </header>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      links: [
+        { title: 'Home', href: '#' },
+        { title: 'About', href: '#' },
+        { title: 'Contact', href: '#' },
+      ],
+    }
+  },
+  methods: {
+    toggleNavbar: function () {
+      const hamburger = document.querySelector('.hamburger')
+      const navMenu = document.querySelector('.nav-menu')
+      hamburger.classList.toggle('active')
+      navMenu.classList.toggle('active')
+    },
+    closeNavbar: function () {
+      const hamburger = document.querySelector('.hamburger')
+      const navMenu = document.querySelector('.nav-menu')
+      hamburger.classList.remove('active')
+      navMenu.classList.remove('active')
+    },
+  },
+}
+</script>
