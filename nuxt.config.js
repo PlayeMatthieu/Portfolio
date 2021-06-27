2f173cafc8051a023514f5cbc2e1f53f5b127bad
Matthieu Playe <matthieu.playe@student.howest.be>
2021-06-27 10:43:34 +0200

    Fix routing

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
   router: {
-    base: '/<repository-name>/'
+    base: '/portfolio/'
   },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
