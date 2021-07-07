5e811aa4a46ccc51c7185a56d577c1e681327122
Matthieu Playe <matthieu.playe@student.howest.be>
2021-07-07 19:24:09 +0200

    Add fetch dynamic projects page

--- pages/_project.vue
+++ pages/_project.vue
@@ -1,13 +1,25 @@
 <template>
   <div>
-    <h1>test</h1>
+    <h1>{{ filteredProject.Project }}</h1>
+    <h2>{{ filteredProject.Img }}</h2>
   </div>
 </template>
 
 <script>
+import projects from '../static/projects.json'
 export default {
-  data() {
-    return {}
+  async asyncData({ params, redirect }) {
+    const Allprojects = projects.Projects
+
+    const filteredProject = Allprojects.find(
+      (el) => el.Project === params.project
+    )
+    console.log(filteredProject)
+    if (filteredProject) {
+      return { filteredProject }
+    } else {
+      redirect('/')
+    }
   },
 }
 </script>
